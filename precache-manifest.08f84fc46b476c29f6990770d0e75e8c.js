a00d0b6d44673bcc921f3f2c86763d032c1bec42
ahmeed2m <2m.fcih@fci.helwan.edu.eg>
2019-12-12 20:34:11 +0200

    Updates

---
+++ precache-manifest.08f84fc46b476c29f6990770d0e75e8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f6d36bb35339bbedc61c4970312410",
+    "url": "/ir-system/index.html"
+  },
+  {
+    "revision": "1a7fd2d93fba343de7a0",
+    "url": "/ir-system/static/css/main.588a1c9b.chunk.css"
+  },
+  {
+    "revision": "03a74b337872683e23c3",
+    "url": "/ir-system/static/js/2.f35af90d.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/ir-system/static/js/2.f35af90d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a7fd2d93fba343de7a0",
+    "url": "/ir-system/static/js/main.7ff318e3.chunk.js"
+  },
+  {
+    "revision": "06aa99db83429aff2bff",
+    "url": "/ir-system/static/js/runtime-main.6341f979.js"
+  },
+  {
+    "revision": "1a7223f77c7944b8306d3b5827a7279e",
+    "url": "/ir-system/static/media/logo.1a7223f7.png"
+  }
+]);
